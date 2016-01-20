5baf349154833ce94fdff7ddf5b9fe383ad1f47a
amirhmz <amdark77@gmail.com>
2016-01-20 18:56:09 +0330

    Update anti_spam.lua

--- plugins/anti_spam.lua
+++ plugins/anti_spam.lua
@@ -1,21 +1,77 @@
---An empty table for solving multiple kicking problem(thanks to @topkecleon )
-kicktable = {}
 
 do
 
+local NUM_MSG_MAX = 8
 local TIME_CHECK = 2 -- seconds
-local data = load_data(_config.moderation.data)
+
+
+
+local function user_print_name(user)
+  if user.print_name then
+    return user.print_name
+  end
+
+  local text = ''
+  if user.first_name then
+    text = user.last_name..' '
+  end
+  if user.lastname then
+    text = text..user.last_name
+  end
+
+  return text
+end
+
+
+
+-- Returns a table with `name` and `msgs`
+local function get_msgs_user_chat(user_id, chat_id)
+  local user_info = {}
+  local uhash = 'user:'..user_id
+  local user = redis:hgetall(uhash)
+  local um_hash = 'msgs:'..user_id..':'..chat_id
+  user_info.msgs = tonumber(redis:get(um_hash) or 0)
+  user_info.name = user_print_name(user)..' ('..user_id..')'
+  return user_info
+end
+
+local function chat_stats(chat_id)
+  -- Users on chat
+  local hash = 'chat:'..chat_id..':users'
+  local users = redis:smembers(hash)
+  local users_info = {}
+
+  -- Get user info
+  for i = 1, #users do
+    local user_id = users[i]
+    local user_info = get_msgs_user_chat(user_id, chat_id)
+    table.insert(users_info, user_info)
+  end
+
+  -- Sort users by msgs number
+  table.sort(users_info, function(a, b) 
+      if a.msgs and b.msgs then
+        return a.msgs > b.msgs
+      end
+    end)
+
+  local text = ''
+  for k,user in pairs(users_info) do
+    text = text..user.name..' => '..user.msgs..'\n'
+  end
+
+  return text
+end
+
 -- Save stats, ban user
 local function pre_process(msg)
   -- Ignore service msg
   if msg.service then
+    print('Service message')
     return msg
   end
-  if msg.from.id == our_id then
-    return msg
-  end
-  
-    -- Save user on Redis
+
+  -- Save user on Redis
   if msg.from.type == 'user' then
     local hash = 'user:'..msg.from.id
     print('Saving user', hash)
@@ -37,90 +93,102 @@ local function pre_process(msg)
     redis:sadd(hash, msg.from.id)
   end
 
-
-
   -- Total user msgs
   local hash = 'msgs:'..msg.from.id..':'..msg.to.id
   redis:incr(hash)
 
-  --Load moderation data
-  local data = load_data(_config.moderation.data)
-  if data[tostring(msg.to.id)] then
-    --Check if flood is one or off
-    if data[tostring(msg.to.id)]['settings']['flood'] == 'no' then
-      return msg
-    end
-  end
-
   -- Check flood
+
+local kick = chat_del_user(chat_id , user_id, ok_cb, true)
+ vardump(kick)
+
   if msg.from.type == 'user' then
     local hash = 'user:'..msg.from.id..':msgs'
     local msgs = tonumber(redis:get(hash) or 0)
-    local data = load_data(_config.moderation.data)
-    local NUM_MSG_MAX = 5
-    if data[tostring(msg.to.id)] then
-      if data[tostring(msg.to.id)]['settings']['flood_msg_max'] then
-        NUM_MSG_MAX = tonumber(data[tostring(msg.to.id)]['settings']['flood_msg_max'])--Obtain group flood sensitivity
-      end
-    end
-    local max_msg = NUM_MSG_MAX * 1
-    if msgs > max_msg then
-      local user = msg.from.id
-      -- Ignore mods,owner and admins
-      if is_momod(msg) then 
-        return msg
-      end
-      local chat = msg.to.id
-      local user = msg.from.id
-      -- Return end if user was kicked before
-      if kicktable[user] == true then
-        return
-      end
-      kick_user(user, chat)
-      local name = user_print_name(msg.from)
-      --save it to log file
-      savelog(msg.to.id, name.." ["..msg.from.id.."] spammed and kicked ! ")
-      -- incr it on redis
-      local gbanspam = 'gban:spam'..msg.from.id
-      redis:incr(gbanspam)
-      local gbanspam = 'gban:spam'..msg.from.id
-      local gbanspamonredis = redis:get(gbanspam)
-      --Check if user has spammed is group more than 4 times  
-      if gbanspamonredis then
-        if tonumber(gbanspamonredis) ==  4 and not is_owner(msg) then
-          --Global ban that user
-          banall_user(msg.from.id)
-          local gbanspam = 'gban:spam'..msg.from.id
-          --reset the counter
-          redis:set(gbanspam, 0)
-          local username = " "
-          if msg.from.username ~= nil then
-            username = msg.from.username
-          end
-          local name = user_print_name(msg.from)
-          --Send this to that chat
-          send_large_msg("chat#id"..msg.to.id, "User [ "..name.." ]"..msg.from.id.." Globally banned (spamming)")
-          local log_group = 1 --set log group caht id
-          --send it to log group
-          send_large_msg("chat#id"..log_group, "User [ "..name.." ] ( @"..username.." )"..msg.from.id.." Globally banned from ( "..msg.to.print_name.." ) [ "..msg.to.id.." ] (spamming)")
-        end
-      end
-      kicktable[user] = true
+    if msgs > NUM_MSG_MAX then
+
+chat_del_user('chat#id'..msg.to.id,'user#id'..msg.from.id,ok_cb,false)
+
+
+
+
+      print('User '..msg.from.id..'is flooding '..msgs)
       msg = nil
     end
     redis:setex(hash, TIME_CHECK, msgs+1)
   end
+
   return msg
 end
 
-local function cron()
-  --clear that table on the top of the plugins
-	kicktable = {}
+local function bot_stats()
+
+  local redis_scan = [[
+    local cursor = '0'
+    local count = 0
+    repeat
+      local r = redis.call("SCAN", cursor, "MATCH", KEYS[1])
+      cursor = r[1]
+      count = count + #r[2]
+    until cursor == '0'
+    return count]]
+
+  -- Users
+  local hash = 'msgs:*:'..our_id
+  local r = redis:eval(redis_scan, 1, hash)
+  local text = 'Users: '..r
+
+  hash = 'chat:*:users'
+  r = redis:eval(redis_scan, 1, hash)
+  text = text..'\nChats: '..r
+
+  return text
+
+end
+
+local function run(msg, matches)
+  if matches[1]:lower() == "stats" then
+
+    if not matches[2] then
+      if msg.to.type == 'chat' then
+        local chat_id = msg.to.id
+        return chat_stats(chat_id)
+      else
+        return 'Stats works only on chats'
+      end
+    end
+
+    if matches[2] == "bot" then
+      if not is_sudo(msg) then
+        return "Bot stats requires privileged user"
+      else
+        return bot_stats()
+      end
+    end
+
+    if matches[2] == "chat" then
+      if not is_sudo(msg) then
+        return "This command requires privileged user"
+      else
+        return chat_stats(matches[3])
+      end
+    end
+  end
 end
 
 return {
-  patterns = {},
-  cron = cron,
+  description = "Plugin to update user stats.", 
+  usage = {
+    "!stats: Returns a list of Username [telegram_id]: msg_num",
+    "!stats chat <chat_id>: Show stats for chat_id",
+    "!stats bot: Shows bot stats (sudo users)"
+  },
+  patterns = {
+    "^!([Ss]tats)$",
+    "^!([Ss]tats) (chat) (%d+)",
+    "^!([Ss]tats) (bot)"
+    }, 
+  run = run,
   pre_process = pre_process
 }
 
