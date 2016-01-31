b197c7140129e1830be119a093f4a17f5a60e8d9
amirhmz <amdark77@gmail.com>
2016-01-31 14:30:46 +0330

    Create lock_join.lua

---
+++ plugins/lock_join.lua
@@ -0,0 +1,22 @@
+if msg.action and msg.action.type then
+    local action = msg.action.type
+    if action == 'chat_add_user_link' then
+      local user_id = msg.from.id
+    if data[tostring(msg.to.id)]['settings']['lock_join'] == 'yes' then
+  if not is_momod(msg) then
+  chat_add_user_link('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+    local msg = 'Join link is Locked !'
+   local receiver = msg.to.id
+    send_large_msg('chat#id'..receiver, msg.."\n", ok_cb, false)
+  
+      end
+   end
+end
+return {
+   description = "to lock joining by link",
+   usage = "!lock join : no one can come to group by link",
+   patterns = {
+      "^!!tgservice (chat_add_user_link)$",
+},
+   run = run
+}
