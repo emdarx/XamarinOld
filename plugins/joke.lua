b5cd76badfc80047f1e9f235af0cf258578d4218
amirhmz <amdark77@gmail.com>
2016-02-04 02:18:17 +0330

    Create joke.lua

---
+++ plugins/joke.lua
@@ -0,0 +1,13 @@
+local database = 'http://umbrella.shayan-soft.ir/txt/'
+local function run(msg)
+	local res = http.request(database.."joke.db")
+	local joke = res:split(",")
+	return joke[math.random(#joke)]
+end
+
+return {
+	description = "500 Persian Joke",
+	usage = "!joke : send random joke",
+	patterns = {"^[/!]joke$"},
+	run = run
+}
