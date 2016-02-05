7d221154e187cdfdb04851876d652ccc95907263
amirhmz <amdark77@gmail.com>
2016-02-05 14:10:52 +0330

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -1,3 +1,4 @@
+-- Auto Leave ( Block and Ban Global) V1.2 For Xamarin Source
 do
 -- Will leave the group if be added
 local function run(msg, matches)
@@ -6,7 +7,7 @@ local bot_id = 172490081 -- your bot id
     if matches[1] == 'leave' and is_admin(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
-      send_large_msg("chat#id"..msg.to.id, '⛔️ This is not one of my groups.', ok_cb, false)
+      send_large_msg("chat#id"..msg.to.id, '⛔️ This is not one of my groups', ok_cb, false)
       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
       block_user("user#id"..msg.from.id,ok_cb,false)
     end
@@ -15,8 +16,12 @@ end
 return {
   patterns = {
     "^[!/](leave)$",
+    "^(leave)$",
     "^!!tgservice (.+)$",
   },
   run = run
+}
+end
+
 }
 end
