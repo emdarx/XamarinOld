1e7804b2ae9f35af3fdfecb5fb7fa5332c6817c5
amirhmz <amdark77@gmail.com>
2016-02-08 14:40:43 +0330

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -1,29 +1,31 @@
  -- FeedBack Plugin For Xamarin Source v1.2
  --https://github.com/amirhmz
+
 do
+
  function run(msg, matches)
- 
-  local fuse = '📌 #فیدبک جدید\n\n👤 نام کاربری : @' .. msg.from.username .. '\n\n👤 نام کاربر : ' .. msg.from.print_name ..'\n\n👤 یوزر آی دی : ' .. msg.from.id .. '\n\n📝 متن پیام : \n' .. matches[1] 
+ local ch = 'chat#id'..msg.to.id
+ local fuse = '📌 #فیدبک جدید\n\n👤 نام کاربر : ' .. msg.from.print_name .. '\n\n👤 نام کاربری : @' .. msg.from.username ..'\n\n👤 کد کاربر : ' .. msg.from.id ..'\n\n👤 کد گروه : '..msg.to.id.. '\n\n📝 متن پیام :\n\n' .. matches[1]
  local fuses = '!printf user#id' .. msg.from.id
- 
+
+
    local text = matches[1]
    local chat = "chat#id"..83535263
-   --like : local chat = "chat#id"..12345678
-   
+
   local sends = send_msg(chat, fuse, ok_cb, false)
   return '✅ Message successfully sent'
- 
+
  end
  end
  return {
-  
+
   description = "Feedback",
- 
-  usage = "!feedback message",
+
+  usage = "feedback: Send A Message To Admins.",
   patterns = {
   "^[!/][Ff]eedback (.*)$",
   "^[Ff]eedback (.*)$"
- 
+  
   },
   run = run
  }
