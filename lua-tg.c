8365cf32311ab492bb6651b6a8ccc93970dafd6e
amirhmz <amdark77@gmail.com>
2016-02-08 15:27:55 +0330

    Added Sticker Support

--- lua-tg.c
+++ lua-tg.c
@@ -1,7 +1,7 @@
 
 /*
     This file is part of telegram-cli.
-
+ 
     Telegram-cli is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 2 of the License, or
