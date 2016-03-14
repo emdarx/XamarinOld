03873afb96cb47d4fca6dee02a7898cd82f40645
amirhmz <amdark77@gmail.com>
2016-03-14 14:20:21 +0330

    Update joke.lua

--- plugins/joke.lua
+++ plugins/joke.lua
@@ -4,7 +4,7 @@
 local database = 'http://vip.opload.ir/vipdl/94/11/amirhmz/'
 local function run(msg)
 	local res = http.request(database.."joke.db")
-	local joke = res:split(",")
+	local joke = res:split(",") 
 	return joke[math.random(#joke)]
 end
 --Joke Plugin v1.0 By @AmirDark
